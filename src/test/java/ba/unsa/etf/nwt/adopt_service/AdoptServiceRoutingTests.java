685f62ee672c9bddda0b56d2cd1957a2d964d615
epita1 <epita1@etf.unsa.ba>
2021-03-28 17:46:40 +0200

    Dodani importi i konfiguracije za MockMVC testove. Preimenovana klasa AdoptServiceRoutingTests

--- src/test/java/ba/unsa/etf/nwt/adopt_service/AdoptServiceRoutingTests.java
+++ src/test/java/ba/unsa/etf/nwt/adopt_service/AdoptServiceRoutingTests.java
@@ -2,7 +2,11 @@ package ba.unsa.etf.nwt.adopt_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -10,7 +14,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class AdoptServiceRoutingTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@ActiveProfiles("test")
+public class AdoptServiceRoutingTests {
     @Autowired
     private MockMvc mockMvc;
 
