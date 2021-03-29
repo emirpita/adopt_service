f499bbead17a3d11b6ae767bc5c517c65f608409
epita1 <epita1@etf.unsa.ba>
2021-03-29 18:16:16 +0200

    Dodane specijalne rute u AddPetRequestController

--- src/main/java/ba/unsa/etf/nwt/adopt_service/controllers/AddPetRequestController.java
+++ src/main/java/ba/unsa/etf/nwt/adopt_service/controllers/AddPetRequestController.java
@@ -4,10 +4,7 @@ import ba.unsa.etf.nwt.adopt_service.models.AddPetRequest;
 import ba.unsa.etf.nwt.adopt_service.response.ResponseMessage;
 import ba.unsa.etf.nwt.adopt_service.services.AddPetRequestService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -25,5 +22,40 @@ public class AddPetRequestController {
     @PostMapping("/add-pet-request")
     public ResponseMessage addAddPetRequest(@Valid @RequestBody AddPetRequest addPetRequest) {
         return addPetRequestService.addAddPetRequest(addPetRequest);
+    }
+
+    @GetMapping("/adoption-request/{userID}")
+    public List<AddPetRequest> getAddPetRequestByUserID(@PathVariable Long userID) {
+        return addPetRequestService.getAddPetRequestByUserID(userID);
+    }
+
+    @GetMapping("/adoption-request/{newPetID}")
+    public List<AddPetRequest> getAddPetRequestByNewPetID(@PathVariable Long newPetID) {
+        return addPetRequestService.getAddPetRequestByNewPetID(newPetID);
+    }
+
+    @GetMapping("/adoption-request/approved")
+    public List<AddPetRequest> getApprovedAddPetRequests() {
+        return addPetRequestService.getApprovedAddPetRequests();
+    }
+
+    @GetMapping("/adoption-request/not-approved")
+    public List<AddPetRequest> getNotApprovedAddPetRequests() {
+        return addPetRequestService.getNotApprovedAddPetRequests();
+    }
+
+    @DeleteMapping("/adoption-request/{id}")
+    public ResponseMessage deleteAddPetRequestByID(@PathVariable Long id) {
+        return addPetRequestService.deleteAddPetRequestByID(id);
+    }
+
+    @DeleteMapping("/adoption-request/{userID}")
+    public ResponseMessage deleteAddPetRequestsByUserID(@PathVariable Long userID) {
+        return addPetRequestService.deleteAddPetRequestsByUserID(userID);
+    }
+
+    @DeleteMapping("/adoption-request/{newPetID}")
+    public ResponseMessage deleteAddPetRequestsByNewPetID(@PathVariable Long newPetID) {
+        return addPetRequestService.deleteAddPetRequestsByNewPetID(newPetID);
     }
 }
--- src/main/java/ba/unsa/etf/nwt/adopt_service/services/AddPetRequestService.java
+++ src/main/java/ba/unsa/etf/nwt/adopt_service/services/AddPetRequestService.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -22,5 +23,97 @@ public class AddPetRequestService {
         addPetRequestRepository.save(addPetRequest);
         return new ResponseMessage(true, HttpStatus.OK, "Request to add a new pet added successfully!");
 
+    }
+
+    public List<AddPetRequest> getAddPetRequestByUserID(Long userID) {
+        return addPetRequestRepository
+                .findAll()
+                .stream()
+                .filter(n -> n.getUserID().equals(userID))
+                .collect(Collectors.toList());
+    }
+
+    public List<AddPetRequest> getAddPetRequestByNewPetID(Long newPetID) {
+        return addPetRequestRepository
+                .findAll()
+                .stream()
+                .filter(n -> n.getNewPetID().equals(newPetID))
+                .collect(Collectors.toList());
+    }
+
+    public List<AddPetRequest> getApprovedAddPetRequests() {
+        return addPetRequestRepository
+                .findAll()
+                .stream()
+                .filter(n -> n.isApproved())
+                .collect(Collectors.toList());
+    }
+
+    public List<AddPetRequest> getNotApprovedAddPetRequests() {
+        return addPetRequestRepository
+                .findAll()
+                .stream()
+                .filter(n -> !n.isApproved())
+                .collect(Collectors.toList());
+    }
+
+    public ResponseMessage deleteAddPetRequestByID(Long id) {
+        try {
+            AddPetRequest addPetRequest = addPetRequestRepository
+                    .findAll()
+                    .stream()
+                    .filter(n -> n.getId().equals(id))
+                    .collect(Collectors.toList()).get(0);
+            addPetRequestRepository.delete(addPetRequest);
+            if (addPetRequestRepository.findById(id) != null)
+                return new ResponseMessage(true, HttpStatus.OK, "Request to add a pet with id=" + id + " deleted successfully!");
+            return new ResponseMessage(false, HttpStatus.NOT_FOUND, "There is no request to add a pet with id=" + id + "!");
+        } catch (Exception e) {
+            return new ResponseMessage(false, HttpStatus.INTERNAL_SERVER_ERROR, "Internal server or database error!");
+        }
+    }
+
+    public ResponseMessage deleteAddPetRequestsByUserID(Long userID) {
+        try {
+            List<AddPetRequest> addPetRequestListUser = addPetRequestRepository
+                    .findAll()
+                    .stream()
+                    .filter(n -> n.getUserID().equals(userID))
+                    .collect(Collectors.toList());
+            for (AddPetRequest addPetRequest : addPetRequestListUser) {
+                addPetRequestRepository.delete(addPetRequest);
+            }
+            if (addPetRequestRepository
+                    .findAll()
+                    .stream()
+                    .filter(n -> n.getUserID().equals(userID))
+                    .collect(Collectors.toList()).get(0) != null)
+                return new ResponseMessage(true, HttpStatus.OK, "Request to add a pet with user id=" + userID + " deleted successfully!");
+            return new ResponseMessage(false, HttpStatus.NOT_FOUND, "There are no requests to add a pet with user id=" + userID + "!");
+        } catch (Exception e) {
+            return new ResponseMessage(false, HttpStatus.INTERNAL_SERVER_ERROR, "Internal server or database error!");
+        }
+    }
+
+    public ResponseMessage deleteAddPetRequestsByNewPetID(Long newPetID) {
+        try {
+            List<AddPetRequest> addPetRequestListPet = addPetRequestRepository
+                    .findAll()
+                    .stream()
+                    .filter(n -> n.getNewPetID().equals(newPetID))
+                    .collect(Collectors.toList());
+            for (AddPetRequest addPetRequest : addPetRequestListPet) {
+                addPetRequestRepository.delete(addPetRequest);
+            }
+            if (addPetRequestRepository
+                    .findAll()
+                    .stream()
+                    .filter(n -> n.getNewPetID().equals(newPetID))
+                    .collect(Collectors.toList()).get(0) != null)
+                return new ResponseMessage(true, HttpStatus.OK, "Request to add a pet with pet id=" + newPetID + " deleted successfully!");
+            return new ResponseMessage(false, HttpStatus.NOT_FOUND, "There are no requests to add a pet with pet id=" + newPetID + "!");
+        } catch (Exception e) {
+            return new ResponseMessage(false, HttpStatus.INTERNAL_SERVER_ERROR, "Internal server or database error!");
+        }
     }
 }
